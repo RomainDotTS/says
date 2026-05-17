de7332592a838abb48a289044a9a6251084a275d
Romain <romain.leroy.pro@outlook.fr>
2026-05-17 19:12:20 +0200

    ci: :construction_worker: Add netlify CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import mdx from '@mdx-js/rollup'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(),mdx()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(),mdx(), netlify()],
 })
 
 export default config
